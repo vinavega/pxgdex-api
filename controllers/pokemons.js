2f233d7dc76d24e2b34577dc7747b0585936f50c
Victor Navega <victor.nevega@invillia.com>
2021-07-24 20:02:17 -0300

    add post

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -1,6 +1,12 @@
 module.exports = (app) => {
+  const pokemon = ["bulbasaur", 2, 3, 4, 5, 6];
+
   app.get("/pokemon", (req, res) => {
-    const pokemon = ["bulbasauro", 2, 3, 4, 5, 6];
     res.send(pokemon);
+  });
+
+  app.post("/pokemon", (req, res) => {
+    pokemon.push(req.body);
+    res.send("Successfully added");
   });
 };
